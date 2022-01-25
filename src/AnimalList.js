1c82e7712b05bea1ce43bd976f9bc35467f3d8a2
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-01-25 16:49:48 -0700

    fixed errors, everything rendering except animal imgs

--- src/AnimalList.js
+++ src/AnimalList.js
@@ -3,9 +3,11 @@ import Animal from './Animal/Animal';
 
 export default function AnimalList(props) {
   
-  return <div>
-    {props.animals.map((animal, i) =>
-      <Animal key={animal.name, i} animal={animal} />)}
+  return (
+    <div>
+      {props.animals.map((animal, i) =>
+        <Animal key={animal.name, i} animal={animal} />)}
     
-  </div>;
+    </div>
+  );
 }
--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Main from './Main/Main';
 import Footer from './Footer';
 import { animals } from './data';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header /> 
       <Main animals={ animals } />
       <Footer />
     </div>
